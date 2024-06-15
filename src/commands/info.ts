53d7bdc66b0dc4cd706d9c5fabe4d0c49c84692f
ohbeomho <ohbeomho0322@gmail.com>
2024-06-15 20:16:31 +0900

    info command

---
+++ src/commands/info.ts
@@ -0,0 +1,19 @@
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { Command } from ".";
+
+const command: Command = {
+  data: new SlashCommandBuilder().setName("info").setDescription("Reqro 봇의 정보를 표시합니다."),
+  func: async (interaction) => {
+    const embed = new EmbedBuilder()
+      .setTitle("Reqro")
+      .setDescription(
+        `
+개발자: **ohbeomho**
+[소스코드](https://github.com/ohbeomho/Reqro)`
+      )
+      .setThumbnail(interaction.client.user.avatarURL());
+    await interaction.reply({ embeds: [embed] });
+  }
+};
+
+export default command;
