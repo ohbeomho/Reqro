3e1940684277290e47fcf0f7fc80e6181714a5ef
ohbeomho <ohbeomho0322@proton.me>
2024-06-16 22:12:24 +0900

    env file path

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-import "dotenv/config";
+import { config } from "dotenv";
+import path from "path";
+config({ path: path.join(__dirname, ".env") });
 
 const { BOT_TOKEN, CLIENT_ID, GUILD_ID, NODE_ENV, REQUEST_CHANNEL_ID, REVIEW_CHANNEL_ID } = process.env;
 export const envVars = { BOT_TOKEN, CLIENT_ID, GUILD_ID, NODE_ENV, REQUEST_CHANNEL_ID, REVIEW_CHANNEL_ID };
