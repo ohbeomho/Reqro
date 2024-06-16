a6a4f908a69b2d3800ef75be10aef73b46725599
ohbeomho <ohbeomho0322@proton.me>
2024-06-16 22:12:55 +0900

    not working when it is compiled into js lol

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -11,9 +11,9 @@ export async function loadCommands(): Promise<Map<string, Command>> {
   const commands = new Map();
 
   for (let file of (await fs.readdir(__dirname, { withFileTypes: true })).filter((d) => d.isFile())) {
-    if (file.name === "index.ts") continue;
+    if (file.name.startsWith("index")) continue;
 
-    const command: Command = (await import(path.join(file.parentPath, file.name))).default;
+    const command: Command = (await import(path.join(file.parentPath, file.name))).command;
     commands.set(command.data.name, command);
   }
 
