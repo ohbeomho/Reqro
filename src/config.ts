53a41928e2950b117cb2d3de11b38390356d4003
ohbeomho <ohbeomho0322@gmail.com>
2024-06-17 18:21:32 +0900

    Will use database to remember request, review channel
    soon

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 import path from "path";
 config({ path: path.join(__dirname, ".env") });
 
-const { BOT_TOKEN, CLIENT_ID, GUILD_ID, NODE_ENV, REQUEST_CHANNEL_ID, REVIEW_CHANNEL_ID } = process.env;
-export const envVars = { BOT_TOKEN, CLIENT_ID, GUILD_ID, NODE_ENV, REQUEST_CHANNEL_ID, REVIEW_CHANNEL_ID };
+const { BOT_TOKEN, CLIENT_ID, NODE_ENV } = process.env;
+export const envVars = { BOT_TOKEN, CLIENT_ID, NODE_ENV };
 
-const { TEST_GUILD_ID, TEST_REQUEST_CHANNEL_ID, TEST_REVIEW_CHANNEL_ID } = process.env;
-export const testEnvVars = { TEST_GUILD_ID, TEST_REQUEST_CHANNEL_ID, TEST_REVIEW_CHANNEL_ID };
+const { TEST_GUILD_ID } = process.env;
+export const testEnvVars = { TEST_GUILD_ID };
